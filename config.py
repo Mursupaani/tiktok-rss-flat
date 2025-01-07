5ddc4b49e2a25c5dce11856cc7170c48e962909f
Mursupaani <antti.pollanen89@gmail.com>
2025-01-07 23:15:43 +0200

    Update config.py

--- config.py
+++ config.py
@@ -1,5 +1,5 @@
 # Set your GH Pages URL here e.g.:
 ghPagesURL = "https://mursupaani.github.io/tiktok-rss-flat/"
-ghRawURL = "https://github.com/Mursupaani/tiktok-rss-flat/tree/main/"
+ghRawURL = "https://raw.githubusercontent.com/Mursupaany/tiktok-rss-flat/main/"
 
 # ghPagesURL = "https://tiktokrss.conoroneill.com/"
